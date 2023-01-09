26726ccca0edadd6089337f2014e315cd9b3adcb
dexigli <aigli@u-bordeaux.fr>
2023-01-09 20:43:26 +0100

    9. Jan 2023
    Updated The seeding & factory processes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'firstName' => $this->faker->firstName(),
+            'lastName' =>$this->faker->lastName(),
+            'birthDate' =>$this->faker->lastName(),
+            'id_entreprise'=>$this->faker->numberBetween(1,40),
+            'phoneNumber'=>$this->faker->phoneNumber(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
